74862dc60d03693d3729fab3f9ad24d503380c59
Jamanul <jk.skb.25@gmail.com>
2024-09-07 11:35:02 +0600

    type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,9 +19,14 @@ export default function Home() {
   function addTwo(num : number){
     return num +2
   }
+
   addTwo(5)
   function isLogged(email:string,name:string,isPaid :boolean){}
   isLogged("email","sakib",true)
+  type User={
+    name: string
+    isLogged :Boolean
+  }
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
