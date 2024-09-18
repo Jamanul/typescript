e79f2ae96cc475f4514aa2412ca5528bb859a294
Jamanul <jk.skb.25@gmail.com>
2024-09-18 13:16:52 +0600

    how to write a better type script function in typesript

--- src/app/functions.ts
+++ src/app/functions.ts
@@ -2,3 +2,12 @@ const addThree =(num:number):number=>{
     return num +3
 }
 addThree(7)
+
+
+function consoleErr(s:string):void {
+    console.log(s)
+}
+
+function handleError (s:string):never{
+    throw new Error('hello')
+}
