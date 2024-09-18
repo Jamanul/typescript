d06c469b35e03992cf98e9bed81b3709ad717387
Jamanul <jk.skb.25@gmail.com>
2024-09-18 17:02:02 +0600

    got to know about type

--- src/app/functions.ts
+++ src/app/functions.ts
@@ -11,3 +11,20 @@ function consoleErr(s:string):void {
 function handleError (s:string):never{
     throw new Error('hello')
 }
+
+function createUser(name:string,price:number):{name:string,price:number}{
+    return {name,price}
+}
+createUser('sakib',100)
+
+type User ={
+    name: string,
+    email: string,
+    isPaid: boolean
+}
+
+const getUser =(user:User):User=>{
+    return {name:user.name,email:user.email,isPaid:user.isPaid}
+}
+
+getUser({name:"L",email:"L",isPaid:true})
