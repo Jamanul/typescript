6a2abb14d0c24fc54b854f75a256988dc8c4e546
Jamanul <jk.skb.25@gmail.com>
2024-09-20 14:39:03 +0600

    got to know about touple

--- src/app/MyUnions.ts
+++ src/app/MyUnions.ts
@@ -15,3 +15,20 @@ sakib = {username:'admin sakib',id:123}
 const stringArray:string[] =['s','a','k'] 
 const numberArray:number[] =[1,2,3] 
 const mixArray:(string | number)[] =[1,2,3,'k'] 
+
+
+// custom data/value
+
+let customSit :"front"| "back" 
+
+customSit ='front'
+customSit ='front'
+
+//touple
+
+let tUser :[number,string,boolean]
+tUser = [11,'sm',true]
+
+type UserT = [number,string]
+
+const user :UserT = [12,'w@gmail.com']
